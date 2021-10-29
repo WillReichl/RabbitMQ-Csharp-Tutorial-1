ce76b9fb13f17c6ddce2334062a4a6ff94a7ee8d
Will Reichl <willreichl@gmail.com>
2021-10-29 12:58:08 -0400

    Add gitignore

---
+++ Receive/Receive.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Receive
+{
+    class Receive
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            
+            channel.QueueDeclare(queue: "hello",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += OnHelloReceived;
+            channel.BasicConsume(queue: "hello",
+                autoAck: true,
+                consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        private static void OnHelloReceived(object model, BasicDeliverEventArgs ea)
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine(" [x] Received {0}", message);
+        }
+    }
+}
---
+++ Send/Send.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Send
+{
+    class Send
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            
+            // The connection abstracts the socket connection, and takes care of protocol version negotiation and 
+            // authentication and so on for us. Here we connect to a RabbitMQ node on the local machine - hence 
+            // the localhost. If we wanted to connect to a node on a different machine we'd simply specify 
+            // its hostname or IP address here.            
+            using var connection = factory.CreateConnection();
+            // Next we create a channel, which is where most of the API for getting things done resides.
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(
+                queue: "hello",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            string message = "Hello World!";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(
+                exchange: "",
+                routingKey: "hello",
+                basicProperties: null,
+                body: body);
+
+            Console.WriteLine(" [x] Sent {0}", message);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
